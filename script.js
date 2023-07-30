84abb8e9b05b8d28685f7f2c2a3ef5a0879676e0
Asif Ekhlaque <132199879+Asifekhlaque@users.noreply.github.com>
2023-07-30 20:53:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector('.btn-1').addEventListener("click",function(){
+    var audio = new Audio("song/bit1.mp3");
+    audio.play();
+})
+
+document.querySelector('.btn-2').addEventListener("click",function(){
+    var audio = new Audio("song/bit2.wav");
+    audio.play();
+})
+
+document.querySelector('.btn-3').addEventListener("click",function(){
+    var audio = new Audio("song/bit3.mp3");
+    audio.play();
+})
+
+document.querySelector('.btn-4').addEventListener("click",function(){
+    var audio = new Audio("song/bit4.wav");
+    audio.play();
+})
+
+document.querySelector('.btn-5').addEventListener("click",function(){
+    var audio = new Audio("song/bit5.mp3");
+    audio.play();
+})
